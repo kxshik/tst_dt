bea033d0339bb39057984805022149208be104f4
swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
2026-05-23 15:30:47 +0530

    vili: Add Flags
    - https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common/commit/ce4afe463ab33a2edac3b73d6f06f27df31372e3
    - https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common/commit/a33da7d5cdff80e1a764eafea25bb66fc8a36ad5
    - https://github.com/Spanish-or-Vanish/android_device_xiaomi_sm8350-common/commit/24f90ebb995bfb208a30fddcc32b154ec9008d04
    
    Change-Id: Id4cf89028afbe08c3061402e60cc1e94c0155f94

--- lineage_vili.mk
+++ lineage_vili.mk
@@ -25,3 +25,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="vili-user 14 UKQ1.231207.002 V816.0.22.0.UKDMIXM release-keys" \
     BuildFingerprint=Xiaomi/vili/vili:14/UKQ1.231207.002/V816.0.22.0.UKDMIXM:user/release-keys
+
+# Flags
+TARGET_INCLUDES_CUSTOM_VOLUMES := true
+TARGET_INCLUDES_DOLBY := true
+TARGET_INCLUDES_ViperFX := true
+
